8c1099dbe0b6ca3e4b7fef625a86ce4c8722ec3a
Vedant Vyas <vedantvyas77@gmail.com>
2025-05-07 20:57:57 +0530

    Create Mergetwosortedlists.cpp

---
+++ Mergetwosortedlists.cpp
@@ -0,0 +1,47 @@
+# Definition for singly-linked list.
+# class ListNode(object):
+#     def __init__(self, val=0, next=None):
+#         self.val = val
+#         self.next = next
+class ListNode:
+    def __init__(self, val=0, next=None):
+        self.val = val
+        self.next = next
+
+class Solution:
+    def mergeTwoLists(self, list1, list2):
+        dummy = ListNode()
+        tail = dummy
+
+        while list1 and list2:
+            if list1.val < list2.val:
+                tail.next = list1
+                list1 = list1.next
+            else:
+                tail.next = list2
+                list2 = list2.next
+            tail = tail.next
+
+        tail.next = list1 if list1 else list2
+        return dummy.next
+
+def build_linked_list(values):
+    dummy = ListNode()
+    current = dummy
+    for val in values:
+        current.next = ListNode(val)
+        current = current.next
+    return dummy.next
+
+def linked_list_to_list(node):
+    result = []
+    while node:
+        result.append(node.val)
+        node = node.next
+    return result
+
+param_1 = build_linked_list([1, 2, 4])
+param_2 = build_linked_list([1, 3, 4])
+
+ret = Solution().mergeTwoLists(param_1, param_2)
+print(linked_list_to_list(ret)) 
